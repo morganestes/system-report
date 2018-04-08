8bfe9b511e4b08927e72da271f82cb93d950a32d
Morgan Estes <morgan.estes@gmail.com>
2018-04-07 22:57:04 -0500

    Use PSR-4 autoloader from Composer
    Not sure how to get around including dev assets yet, but I'm working on
    it. For now, just commit the parts that I know we need - namely the
    autoloader and production-ready parts of Composer.

---
+++ src/Settings.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Admin UI Settings page
+ *
+ * @package MorganEstes\SystemReport
+ */
+
+namespace MorganEstes\SystemReport;
+
+/**
+ * Class Settings
+ *
+ * @package MorganEstes\SystemReport
+ */
+class Settings {
+	public function __construct() {
+	}
+
+	public function __clone() {
+		// TODO: Implement __clone() method.
+	}
+
+	public function init() {
+		add_action( 'admin_notices', function () {
+			?>
+			<div class="notice notice-info is-dismissible">
+				<p><?php esc_html_e( 'Hi from Morgan\'s System Reports!', 'morgan-am-system-report' ); ?>
+				</p>
+			</div>
+			<?php
+		} );
+	}
+
+	public static function runner() {
+		$me = new self();
+		$me->init();
+	}
+}
---
+++ src/functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: morganestes
+ * Date: 4/7/18
+ * Time: 10:38 PM
+ */
+
+namespace MorganEstes\SystemReport;
+
+function foo() {
+	return 'bar';
+}
+
+add_action( 'admin_init', [ __NAMESPACE__ . '\\Settings', 'runner' ] );
