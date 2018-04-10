012c23c6dc3ec140bbb1fa622bb44aeace2cdd35
Morgan Estes <morgan.estes@gmail.com>
2018-04-09 21:04:27 -0500

    Remove placeholder code
    Cruft left in to test certain features, but no longer needed as we move
    forward. Begone!

--- src/functions.php
+++ src/functions.php
@@ -1,15 +1,8 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: morganestes
- * Date: 4/7/18
- * Time: 10:38 PM
+ * General plugin functions
+ *
+ * @package MorganEstes\SystemReport
  */
 
 namespace MorganEstes\SystemReport;
-
-function foo() {
-	return 'bar';
-}
-
-\add_action( 'admin_init', [ __NAMESPACE__ . '\\Settings', 'runner' ] );
